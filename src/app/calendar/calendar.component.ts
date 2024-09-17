cf202df59e65d75f138a7c73245cc67f8a7de69d
DESKTOP-TN3VE9B\szymo <szymonzaba75@gmail.com>
2024-09-17 20:03:36 +0200

    update

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { EventService, Event } from '../services/event.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router'; 
 
 @Component({
   selector: 'app-calendar',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css'],
 })
